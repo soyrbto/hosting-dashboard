2d7765f303f848a2734373ef90ab34ce74e3276b
soyrbto <soyrbto@gmail.com>
2021-08-07 16:12:48 -0400

    setup: desktop; header, tab, panel

--- src/Layouts/desktop/desktop.svelte
+++ src/Layouts/desktop/desktop.svelte
@@ -1,5 +1,20 @@
-<!-- 
-    prop:
+<script>
+  import Header from './header.svelte';
+  import PanelTab from './panelTab.svelte';
+  import Dashboard from './dashboard.svelte';
 
-    account name: string / fetch => store
- -->
+  let tab = Dashboard;
+
+  //   prop:
+  // account name: string / fetch => store
+</script>
+
+<Header />
+<main>
+  <PanelTab />
+  <svelte:component this={tab} />
+</main>
+
+<style lang="scss">
+  //some styles
+</style>
