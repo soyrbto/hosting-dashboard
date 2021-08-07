6901bbb1e63019278504a27b0bba679561a2cd33
soyrbto <soyrbto@gmail.com>
2021-08-07 12:10:32 -0400

    update

--- src/staticContent.js
+++ src/staticContent.js
@@ -1,15 +1,14 @@
-const panelTab,
-dashboard,
-cloudServer,
-dedicatedServer,
-dedicatedOrder,
-cloudOrder,
-dedicatedTable,
-cloudTable,
-account,
-support,
-header, ;
-
+let panelTab,
+  dashboard,
+  cloudServer,
+  dedicatedServer,
+  dedicatedOrder,
+  cloudOrder,
+  dedicatedTable,
+  cloudTable,
+  account,
+  support,
+  header;
 
 panelTab = {
   top: ['Dashboard', 'cloud server', 'dedicated server'],
@@ -18,8 +17,18 @@ panelTab = {
 
 dashboard = {
   welcomeM: 'Welcome to the control panel',
-  notifT: 'notification',
   pendingT: 'pending renewal',
+  pendingC:
+    'Lorem ipsum dolor sit amet consectetur adipiscing elit, congue sodales.',
+  promotionT: 'Stay tuned!',
+  promotionC:
+    'keep an aye on the dashboard and the email to get to know our latest promotions',
+
+  Notification: {
+    title: 'notification',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis sociis metus eros ornare laoreet vestibulum ut, tempus dui curabitur dis congue sodales.',
+  },
 };
 
 cloudServer = {
@@ -52,90 +61,92 @@ cloudOrder = {
   byeText: `Press the "Create Server" button after verifying that all the fields are correct. You can remove this server whenever you want, so don't worry!`,
 };
 
-account ={
-    title: 'Account',
+account = {
+  title: 'Account',
 };
 
-header ={
-    name: 'Sergio',
+header = {
+  name: 'Sergio',
 };
 
-support ={
-    title: 'Support',
+support = {
+  title: 'Support',
 };
 
-dedicatedTable = [{
+dedicatedTable = [
+  {
     label: 'label 1',
     system: 'linux001',
-    price:'150$',
+    price: '150$',
     location: 'Spain',
-    status: true
-},
-{
+    status: true,
+  },
+  {
     label: 'label 2',
     system: 'linux002',
-    price:'250$',
+    price: '250$',
     location: 'Sweden',
-    status: false
-},
-{
+    status: false,
+  },
+  {
     label: 'label 3',
     system: 'linux003',
-    price:'410$',
+    price: '410$',
     location: 'Russia',
-    status: true
-},
-{
+    status: true,
+  },
+  {
     label: 'label 4',
     system: 'linux004',
-    price:'160$',
+    price: '160$',
     location: 'India',
-    status: true
-}];
+    status: true,
+  },
+];
 
-cloudTable = [{
+cloudTable = [
+  {
     label: 'label 1',
     system: 'linux001',
-    price:'150$',
+    price: '150$',
     location: 'Spain',
-    status: true
-},
-{
+    status: true,
+  },
+  {
     label: 'label 2',
     system: 'linux002',
-    price:'250$',
+    price: '250$',
     location: 'Sweden',
-    status: false
-},
-{
+    status: false,
+  },
+  {
     label: 'label 3',
     system: 'linux003',
-    price:'410$',
+    price: '410$',
     location: 'Russia',
-    status: true
-},
-{
+    status: true,
+  },
+  {
     label: 'label 4',
     system: 'linux004',
-    price:'160$',
+    price: '160$',
     location: 'India',
-    status: true
-}];
-
+    status: true,
+  },
+];
 
 const content = {
-    panelTab: panelTab,
-    dashboard: dashboard,
-    cloudServer: cloudServer,
-    dedicatedServer: dedicatedServer,
-    cloudOrder: cloudOrder,
-    dedicatedOrder:dedicatedOrder,
-    dedicatedTable: dedicatedTable,
-    cloudTable: cloudTable,
-    account: account,
-    support: support,
-    header: header,
-}
+  panelTab: panelTab,
+  dashboard: dashboard,
+  cloudServer: cloudServer,
+  dedicatedServer: dedicatedServer,
+  cloudOrder: cloudOrder,
+  dedicatedOrder: dedicatedOrder,
+  dedicatedTable: dedicatedTable,
+  cloudTable: cloudTable,
+  account: account,
+  support: support,
+  header: header,
+};
 
-
-export  default content;
+export default content;
