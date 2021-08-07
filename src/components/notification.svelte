25ad0271b45ea698bbbeba8e0725b9cb12074bf0
soyrbto <soyrbto@gmail.com>
2021-08-07 10:17:47 -0400

    update

--- src/components/notification.svelte
+++ src/components/notification.svelte
@@ -3,5 +3,6 @@
 
     title: title => (string) / static
     content: promotion info => (HTML) / fetch 
+    link: url to go to 
     button.svelte
  -->
--- src/components/promotions.svelte
+++ src/components/promotions.svelte
@@ -4,5 +4,6 @@
     title: promotion title => (string) / static
     image: PROMOTIONAL BANNER => (URL) / fetch
     content: promotion info => (HTML)  / fetch
+    link: url to go to 
     button.svelte
  -->
