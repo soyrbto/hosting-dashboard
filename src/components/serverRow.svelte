7528b8cb67a7c4240fe44203a9e798d210bcd962
soyrbto <soyrbto@gmail.com>
2021-08-22 19:19:35 -0400

    update

--- src/components/serverRow.svelte
+++ src/components/serverRow.svelte
@@ -1,42 +1,47 @@
 <script>
   import { slide } from 'svelte/transition';
   import Button from '../components/button.svelte';
+
+  export let lastRow = false;
   export let title = false;
-  export let content = {};
+  export let content = {
+    price: 'Price',
+    label: 'label',
+    location: 'Location',
+    status: 'Status',
+    actions: 'Actions',
+    details: 'Details',
+  };
+
+  $: visiblePassword = false;
 
   let open = false;
 </script>
 
-{#if title}
-  <div class="component-wrapper">
-    <div class="row title">
-      <div class="label">Label</div>
-      <div class="price">Price</div>
-      <div class="location">Location</div>
-      <div class="status">Status</div>
-      <div class="actions">Actions</div>
-      <div class="details" />
+<div class="component-wrapper" class:title class:last-row={lastRow}>
+  <div class="row">
+    <div class="label">
+      {content.label}
+      <span>{content.system == undefined ? '' : content.system}</span>
     </div>
-  </div>
-{/if}
-
-{#if !title}
-  <div class="component-wrapper">
-    <div class="row">
-      <div class="label">
-        {content.label} <span>{content.system}</span>
-      </div>
-      <div class="price">{content.price}</div>
-      <div class="location">{content.location}</div>
-      <div class="status {content.status ? 'active' : 'inactive'}">
-        {content.status ? 'Active' : 'Inactive'}
-      </div>
-      <div class="actions">
+    <div class="price">{content.price}</div>
+    <div class="location">{content.location}</div>
+    <div
+      class="status {!title ? (content.status ? 'active' : 'inactive') : ''}"
+    >
+      {!title ? (content.status ? 'Active' : 'Inactive') : 'Status'}
+    </div>
+    <div class="actions">
+      {#if !title}
         <img src="./img/stopIcon.svg" alt="" />
         <img src="./img/restartIcon.svg" alt="" />
         <img src="./img/reinstallIcon.svg" alt="" />
-      </div>
-      <div class="details">
+      {:else}
+        Actions
+      {/if}
+    </div>
+    <div class="details">
+      {#if !title}
         <img
           on:click={() => {
             open = !open;
@@ -44,46 +49,56 @@
           src="./img/detailsIcon.svg"
           alt=""
         />
-      </div>
+      {/if}
     </div>
-
-    {#if open}
-      <div class="row subtable" transition:slide>
-        <div>
-          IP address
-          <span> 192.256.65.1 </span>
-        </div>
-        <div>
-          aditional IPs
-          <span> 192.256.65.1 </span>
-          <span> 192.256.65.1 </span>
-        </div>
-        <div>
-          Password
-          <span> <input type="password" /> </span>
-        </div>
-        <div>
-          OS
-          <span> Linux 001 </span>
-        </div>
-        <div class="button-wrapper">
-          <Button
-            secundary={true}
-            padding={`6px 20px`}
-            content={'Clone Server'}
-          />
-        </div>
-      </div>
-    {/if}
   </div>
-{/if}
+
+  {#if open}
+    <div class="row subtable" transition:slide>
+      <div>
+        IP address
+        <span> 192.256.65.1 </span>
+      </div>
+      <div>
+        aditional IPs
+        <span> 192.256.65.1 </span>
+        <span> 192.256.65.1 </span>
+      </div>
+      <div>
+        Password
+        <span>
+          <input type={visiblePassword ? 'text' : 'password'} />
+          <img
+            src="./img/show-password.svg"
+            alt="show password toggle"
+            on:click={() => (visiblePassword = !visiblePassword)}
+          /></span
+        >
+      </div>
+      <div>
+        OS
+        <span> Linux 001 <img src="./img/reinstall-icon.svg" alt="" /> </span>
+      </div>
+      <div class="button-wrapper">
+        <Button
+          secundary={true}
+          padding={`6px 20px`}
+          content={'Clone Server'}
+        />
+      </div>
+      <div class="details" />
+    </div>
+  {/if}
+</div>
 
 <style lang="scss">
   .component-wrapper {
     .row {
+      border-bottom: solid 1px #bcbcbc;
       height: 61px;
       padding: 0px 40px;
       display: flex;
+      flex-grow: 1;
 
       .details {
         width: 2%;
@@ -123,8 +138,12 @@
           font-weight: 400;
           font-size: flexUnit(12px);
 
+          img {
+            margin-left: 10px;
+          }
+
           input {
-            width: 80%;
+            width: 70%;
           }
         }
       }
@@ -133,15 +152,15 @@
     .subtable {
       background-color: #eaf3fe;
       height: auto;
-      padding: 5px 40px;
+      padding: 0px 40px;
 
       div {
         padding: 10px 0px;
       }
 
       .button-wrapper {
-        width: 176px;
-        height: 37px;
+        height: auto;
+        padding: 25px 0;
       }
     }
 
@@ -154,6 +173,12 @@
         font-weight: 700;
         font-size: flexUnit(22px);
       }
+    }
+  }
+
+  .component-wrapper.last-row {
+    .row {
+      border-bottom: none;
     }
   }
 </style>
